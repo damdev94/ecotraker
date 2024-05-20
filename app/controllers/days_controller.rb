5a1dc4ff3e53efda23162aad2fd02eb9081a7f24
damien duport percier <damienviel69@gmail.com>
2024-05-20 16:49:14 -0400

    assigner le système de jours

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -3,4 +3,14 @@ class DaysController < ApplicationController
     today = Date.today
     @trips = Trip.joins(:days).where(days: {date: today}).distinct
   end
+
+  def create
+    @day = Day.new(day_params)
+  end
+
+  private
+
+  def day_params
+    params.require(:day).permit(:date, :trip_id)
+  end
 end

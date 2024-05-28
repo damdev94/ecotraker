0c9e90bbf8a05ce4725f87999f44ab819e361b11
damien duport percier <damienviel69@gmail.com>
2024-05-28 19:51:05 -0400

    beug de redirection new place corrigé

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,18 +4,21 @@ class PlacesController < ApplicationController
   def index
     @places = Place.all
   end
-  
+
   def new
     @place = Place.new
+    flash[:notice] = session.delete(:place_create_notice)
+    flash[:alert] = session.delete(:place_create_alert)
   end
 
   def create
     @place = Place.new(place_params)
     @place.user_id = current_user.id
     if @place.save
-      redirect_to new_vehicle_path
+      session[:place_create_notice] = "Place was successfully added."
+      redirect_to new_place_path
     else
-      flash[:alert] = "There was a problem adding the place."
+      session[:place_create_alert] = "There was a problem adding the place."
       render :new
     end
   end

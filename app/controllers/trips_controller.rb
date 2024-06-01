9eb391061a3097997b4d0f16af7b2418459c8dd6
damien duport percier <damienviel69@gmail.com>
2024-06-01 16:45:28 -0400

    checkbox css ok

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -53,7 +53,7 @@ class TripsController < ApplicationController
 
     if @trip.update(trip_params)
       @trip.days.destroy_all
-      params[:trip][:schedule].each do |day|
+      params[:trip][:schedule].values.each do |day|
         Day.create(date: Date.parse(day), trip: @trip)
       end
       @trip.label = "#{start_place.name} to #{end_place.name}"

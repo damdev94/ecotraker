828c8ddf5f4806ea21e19d876bc6253630012bed
Zephir <zephbrk33@gmail.com>
2024-06-13 21:30:35 -0400

    Current_user_log

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    if user_signed_in?
+      @user = current_user
+    else
+      redirect_to new_user_session_path
+    end
 
   end
 
